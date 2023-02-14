86397828241c59aaa27ebb526b9c2a947fd637a0
RobMico <wegasd10@gmail.com>
2023-02-14 18:34:22 +0200

    linter, prettier setup

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "commonjs": true,
+        "es2021": true
+    },
+    "extends": "eslint:recommended",
+    "overrides": [
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest"
+    },
+    "rules": {
+        "no-var": "error",
+        "semi":"warn",
+        "no-multi-spaces": "warn",
+        "space-in-parens": "error",
+        "no-multiple-empty-lines": "warn",
+        "prefer-const": "warn",
+        "no-use-before-define": "error",
+        "no-unused-vars":"warn"
+    }
+};
