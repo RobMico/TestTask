c0f9c523437a26bb10b7655352cee085239256c4
RobMico <wegasd10@gmail.com>
2023-02-14 21:08:53 +0200

    not null added

--- migrations/20230214181943-CreateUserTable.js
+++ migrations/20230214181943-CreateUserTable.js
@@ -2,9 +2,9 @@ module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.createTable('users', {
       id: { type: Sequelize.INTEGER, primaryKey: true, autoIncrement: true },
-      email: { type: Sequelize.STRING, unique: true },
-      password: { type: Sequelize.STRING },
-      name: { type: Sequelize.STRING },
+      email: { type: Sequelize.STRING, unique: true, allowNull:false},
+      password: { type: Sequelize.STRING, allowNull:false},
+      name: { type: Sequelize.STRING, allowNull:false},
       createdAt: {
         type: Sequelize.DATE,
         defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
--- models/user.js
+++ models/user.js
@@ -3,9 +3,9 @@ const {DataTypes} = require('sequelize');
 
 const User = sequelize.define('user', {
     id:{type: DataTypes.INTEGER, primaryKey:true, autoIncrement:true},
-    email:{type: DataTypes.STRING, unique:true},
-    password:{type: DataTypes.STRING},
-    name:{type: DataTypes.STRING}
+    email:{type: DataTypes.STRING, unique:true, allowNull:false},
+    password:{type: DataTypes.STRING, allowNull:false},
+    name:{type: DataTypes.STRING, allowNull:false}
 });
 
 module.exports = User;
