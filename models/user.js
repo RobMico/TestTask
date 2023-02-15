6a5a599a9f4339b214f70950dd6d3bcd78503530
RobMico <wegasd10@gmail.com>
2023-02-15 14:18:34 +0200

    sequelize validation

--- models/user.js
+++ models/user.js
@@ -3,9 +3,9 @@ const { DataTypes } = require('sequelize');
 
 const User = sequelize.define('user', {
     id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
-    email: { type: DataTypes.STRING, unique: true, allowNull: false },
+    email: { type: DataTypes.STRING, unique: true, allowNull: false, validate: { isEmail: true } },
     password: { type: DataTypes.TEXT, allowNull: false },
-    name: { type: DataTypes.STRING, allowNull: false },
+    name: { type: DataTypes.STRING, allowNull: false, validate: { isAlpha: true, len: [2, 30] } },
     createdAt: { type: DataTypes.DATE },
     updatedAt: { type: DataTypes.DATE }
 }, { createdAt: false, updatedAt: false });
